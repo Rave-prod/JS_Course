0ac8c60ed0c6d195e257958ba322f13307564d83
Rave-prod <57764729+Rave-prod@users.noreply.github.com>
2019-12-04 16:37:23 +0500

    Add files via upload

---
+++ Sprite/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import '../styles/App.styl'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="button"></div>
+                <div className="button1"></div>
+                <div className="button2"></div>
+                <div className="button3"></div>
+                <div className="button4"></div>
+                <div className="button5"></div>
+            </div>
+        )
+    }
+}
+
+export default App;
